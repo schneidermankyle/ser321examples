7a78925b0f4127c24987729eb82519c60cee940f
Kyle Sch <kyle.schneiderman@asu.edu>
2020-10-29 00:08:18 -0400

    Update multiply exception handling

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -217,7 +217,7 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Result is: " + result);
-          } catch {
+          } catch (Exception e) {
             // Generate response
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
